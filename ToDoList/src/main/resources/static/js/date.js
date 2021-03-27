5778a5985960b2a32ca0c21b48161b9b032b671c
William <buiquanghuy.10122000.10@gmail.com>
2021-03-27 13:16:45 +0700

    feat: Add date.js to js resources

---
+++ ToDoList/src/main/resources/static/js/date.js
@@ -0,0 +1,11 @@
+function DateCheck() {
+    var StartDate= document.getElementById('txtStartDate').value;
+    var EndDate= document.getElementById('txtEndDate').value;
+    var eDate = new Date(EndDate);
+    var sDate = new Date(StartDate);
+    if(StartDate!= '' && StartDate!= '' && sDate> eDate)
+    {
+        alert("Ngày kết thúc phải lớn hơn ngày bắt đầu");
+        return false;
+    }
+}
